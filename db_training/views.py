16f2c6f06260eeab846963d78142945b6abc2f2e
LamB <a2c.ambivalent@outlook.jp>
2021-03-30 15:17:20 +0900

    sqlクエリを直接書く

--- db_training/views.py
+++ db_training/views.py
@@ -6,11 +6,24 @@ from .forms import FindForm
 from .forms import Friend
 from .models import Friend
 from django.shortcuts import redirect
+from django.db.models import Count, Sum, Avg, Min, Max
 
 def index(request):
-    data = Friend.objects.all()
+    data = Friend.objects.all().order_by('id')
+    re1 = Friend.objects.aggregate(Count('age'))
+    re2 = Friend.objects.aggregate(Sum('age'))
+    re3 = Friend.objects.aggregate(Avg('age'))
+    re4 = Friend.objects.aggregate(Min('age'))
+    re5 = Friend.objects.aggregate(Max('age'))
+    msg = 'count:' + str(re1['age__count']) \
+        +'<br>Sum:' + str(re2['age__sum']) \
+        +'<br>Average:' + str(re3['age__avg']) \
+        +'<br>Min:' + str(re4['age__min']) \
+        +'<br>Max:' + str(re5['age__max']) 
+
     params = {
             'title': 'Hello',
+            'message': msg,
             'data': data,
     }
 
@@ -61,10 +74,13 @@ def delete(request, num):
 
 def find(request):
     if(request.method == 'POST'):
-        msg = '結果'
+        msg = request.POST['find']
         form = FindForm(request.POST)
-        str = request.POST['find']
-        data = Friend.objects.filter(name__contains=str)
+        sql = 'SELECT * FROM db_training_friend'
+        if (msg != ''):
+            sql += ' WHERE ' + msg
+        data = Friend.objects.raw(sql)
+        msg = sql
     else:
         msg = 'なにさがしてるん'
         form = FindForm()
