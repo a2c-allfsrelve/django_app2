9b7181ed5a77dad678aa97711a054c5146312a40
LamB <a2c.ambivalent@outlook.jp>
2021-04-11 16:20:34 +0900

    p285まで

--- db_training/forms.py
+++ db_training/forms.py
@@ -10,6 +10,28 @@ class FriendForm(forms.ModelForm):
 class FindForm(forms.Form):
     find = forms.CharField(label='Find', required=False)
 
+class CheckForm(forms.Form):
+    # empty = forms.CharField(label='empty', empty_value=True)
+    # min = forms.CharField(label='min', min_length=10)
+    # max = forms.CharField(label='max', max_length=10)
+    # required = forms.IntegerField(label='required')
+    # minnum = forms.IntegerField(label='MinNum', min_value=10)
+    # maxnum = forms.IntegerField(label='MaxNum', max_value=10)
+
+    # date = forms.DateField(label='Date', input_formats=['%d'])
+    # time = forms.TimeField(label='Time')
+    # datetime = forms.DateTimeField(label='DateTime')
+
+    str = forms.CharField(label='String')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        str = cleaned_data['str']
+        if(str.lower().startswith('no')):
+            raise forms.ValidationError('You input No!!!')
+
+
+
 
 
 # class HelloForm(forms.Form):
--- db_training/models.py
+++ db_training/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
 
 class Friend(models.Model):
-    name = models.CharField(max_length=100)
-    mail = models.EmailField(max_length=200)
+    name = models.CharField(max_length=10)
+    mail = models.EmailField(max_length=100, validators=[
+        RegexValidator(r'^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$')]) #バリデーションルールに正規表現を用いる
     gender = models.BooleanField()
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(default=0, validators=[
+        MinValueValidator(0),
+        MaxValueValidator(150)]) #モデルで最初からバリデーションを設定
     birthday = models.DateField()
 
     #カラムをテキストの値に変換したもの
